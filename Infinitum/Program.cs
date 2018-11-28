0a2d19550b4b6766d9d5f027f93cbba1f622446a
Telorath <TelorathSennith@gmail.com>
2018-11-27 20:38:02 -0500

    Refactored menu to be easier to understand

--- Infinitum/Program.cs
+++ Infinitum/Program.cs
@@ -8,68 +8,92 @@ namespace Infinitum
 {
     class Program
     {
+        static List<string> options = new List<string>();
+        static int selectedOption = 0;
+        static bool playing = true;
+
+        public static int SelectedOption
+        {
+            get { return selectedOption; }
+            set {
+                selectedOption = value;
+                if (selectedOption >= options.Count)
+                {
+                    selectedOption = 0;
+                }
+                if (selectedOption < 0)
+                {
+                    selectedOption = options.Count - 1;
+                }
+            }
+        }
+
+        static void Display()
+        {
+            Console.Clear();
+            for (int i = 0; i < options.Count; i++)
+            {
+                if (i == selectedOption)
+                {
+                    Console.Write(">");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(options[i]);
+            }
+        }
+
+        static void SelectOption()
+        {
+                if (selectedOption == 0)
+                {
+                    Console.WriteLine("Sorry! No game to play!");
+                    Console.ReadLine();
+                }
+
+                if (selectedOption == 1)
+                {
+                    playing = false;
+                    Console.WriteLine("Thanks for playing!");
+                    Console.ReadLine();
+                    return;
+                }
+        }
+
+        static void GetInput()
+        {
+            ConsoleKeyInfo keyPress = Console.ReadKey();
+            if (keyPress.Key == ConsoleKey.UpArrow)
+            {
+                SelectedOption--;
+            }
+            else if (keyPress.Key == ConsoleKey.DownArrow)
+            {
+                SelectedOption++;
+            }
+            else if (keyPress.Key == ConsoleKey.Enter)
+            {
+
+                SelectOption();
+
+            }
+        }
+
         static void Main(string[] args)
         {
-            List<string> options = new List<string>();
-
             options.Add("Play");
             options.Add("Quit");
 
-            int selectedOption = 0;
-
             Console.CursorVisible = false;
 
-            while (true)
+            while (playing)
             {
 
-                Console.Clear();
-                for (int i = 0; i < options.Count; i++)
-                {
-                    if (i == selectedOption)
-                    {
-                        Console.Write(">");
-                    }
-                    else
-                    {
-                        Console.Write(" ");
-                    }
-                    Console.WriteLine(options[i]);
-                }
+                Display();
 
-                ConsoleKeyInfo keyPress = Console.ReadKey();
-                if (keyPress.Key == ConsoleKey.UpArrow)
-                {
-                    selectedOption--;
-                    if (selectedOption < 0)
-                    {
-                        selectedOption = options.Count - 1;
-                    }
-                }
-                else if (keyPress.Key == ConsoleKey.DownArrow)
-                {
-                    selectedOption++;
-                    if (selectedOption >= options.Count)
-                    {
-                        selectedOption = 0;
-                    }
-                }
-                else if (keyPress.Key == ConsoleKey.Enter)
-                {
-
-                    if (selectedOption == 0)
-                    {
-                        Console.WriteLine("Sorry! No game to play!");
-                        Console.ReadLine();
-                    }
-
-                    if (selectedOption == 1)
-                    {
-                        Console.WriteLine("Thanks for playing!");
-                        Console.ReadLine();
-                        return;
-                    }
-
-                }
+                GetInput();
 
             }
 
