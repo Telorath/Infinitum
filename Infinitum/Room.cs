1f0bea75749a6ac0925e7a6d44691372c7add2b6
Telorath <TelorathSennith@gmail.com>
2018-11-27 19:57:56 -0500

    Added a room class with a position

---
+++ Infinitum/Room.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infinitum
+{
+    class Room
+    {
+        public int x;
+        public int y;
+    }
+}
